ec501ed283cec7cc2df6a29f2fb7c7e4554efece
sanjeevkomma <sanjeevkomma@gmail.com>
2021-10-24 20:06:44 +0530

    spring yml

---
+++ spring-yaml/src/main/java/com/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+import com.demo.config.GlobalProperties;
+import com.demo.config.WordpressProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+    private WordpressProperties wpProperties;
+
+    @Autowired
+    private GlobalProperties globalProperties;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println(globalProperties);
+        System.out.println(wpProperties);
+    }
+
+}
