0abe5b06acb17320e8ac8d854b2b5c891da1eb87
sanjeevkomma <sanjeevkomma@gmail.com>
2021-10-30 23:00:54 +0530

    spring boot h2 db

---
+++ spring-boot-h2-database/src/main/java/com/demo/SpringBootH2DatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootH2DatabaseApplication.class, args);
+	}
+
+}
