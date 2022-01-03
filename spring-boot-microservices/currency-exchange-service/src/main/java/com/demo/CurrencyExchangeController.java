e435657164aea477bb61f60ce007d10fa6ab661f
sanjeevkomma <sanjeevkomma@gmail.com>
2022-01-03 23:47:47 +0530

    inital commit

--- spring-boot-microservices/currency-exchange-service/src/main/java/com/demo/CurrencyExchangeController.java
+++ spring-boot-microservices/currency-exchange-service/src/main/java/com/demo/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.demo;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	
 	@Autowired
 	private CurrencyExchangeRepository repository;
@@ -21,6 +23,9 @@ public class CurrencyExchangeController {
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+		
 		CurrencyExchange currencyExchange 
 					= repository.findByFromAndTo(from, to);
 		
