6b2337a69ae4edc22d9c119755d928ed6994329e
sanjeevkomma <sanjeevkomma@gmail.com>
2022-02-07 19:58:49 +0530

    in

---
+++ spring-boot-data-api/src/main/java/com/demo/SpringBootDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-retry/src/main/java/com/demo/SpringBootRetryApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRetryApplication.class, args);
+	}
+
+}
