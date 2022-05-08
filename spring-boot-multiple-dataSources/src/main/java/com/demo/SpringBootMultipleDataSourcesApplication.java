6bb87e6463bda08b6ecf77a03d6834a19fa49f1b
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-05-08 12:08:10 +0530

    in

---
+++ spring-boot-multiple-dataSources/src/main/java/com/demo/SpringBootMultipleDataSourcesApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleDataSourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleDataSourcesApplication.class, args);
+	}
+
+}
