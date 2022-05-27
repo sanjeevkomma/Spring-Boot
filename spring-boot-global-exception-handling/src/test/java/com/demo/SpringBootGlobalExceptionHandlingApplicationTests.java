208dec98bb7d42d53a4248ad8e89f76ed27bfbe7
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-05-27 09:43:28 +0530

    in

---
+++ spring-boot-global-exception-handling/src/test/java/com/demo/SpringBootGlobalExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGlobalExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
