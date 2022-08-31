414810cf60eee97fa6d718fb716d1ff607f7a930
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-08-31 18:27:47 +0530

    in

--- spring-boot-Gradle/src/main/java/com/demo/Test.java
+++ spring-boot-Gradle/src/main/java/com/demo/Test.java
@@ -1,12 +1,29 @@
 package com.demo;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Test {
 	
 	public void cal(int a, int b) {
 		
 		int c = a + b;
 		
-		System.out.println(c)
+		int arr[] = { 1,2,3,4,5 };
+		
+		System.out.println(c);
+		
+		
+		Stack ff; 
+		
+		Queue fff;
+		
+		LinkedList jj; 
+		
+		Arrays jjl;
 	  
 	}
 
