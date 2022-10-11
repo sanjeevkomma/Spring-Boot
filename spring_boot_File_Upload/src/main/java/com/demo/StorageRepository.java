b92ce62b1f353f7af2cc7aab607265b284a8c0c6
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-10-11 22:27:14 +0530

    in

--- spring_boot_File_Upload/src/main/java/com/demo/StorageRepository.java
+++ spring_boot_File_Upload/src/main/java/com/demo/StorageRepository.java
@@ -3,7 +3,9 @@ package com.demo;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StorageRepository extends JpaRepository<ImageData,Long> {
 
     Optional<ImageData> findByName(String fileName);
