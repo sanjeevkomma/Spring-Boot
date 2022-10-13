cc0bf02c04e604ef5524fd49dbd6fe7b96e00129
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-10-13 22:10:50 +0530

    in

--- spring_boot_File_Upload/src/main/java/com/demo/SpringBootFileUploadApplication.java
+++ spring_boot_File_Upload/src/main/java/com/demo/SpringBootFileUploadApplication.java
@@ -2,12 +2,28 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class SpringBootFileUploadApplication {
+public class SpringBootFileUploadApplication implements CommandLineRunner {
+	
+	@Value("${user.name}")
+    private String name;
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootFileUploadApplication.class, args);
+		
 	}
+	
+	 @Override
+	    public void run(String... args) {
+	    	
+	        System.out.println("user name is :: " + name);
+	        
+	    }
+	
+	
 
 }
