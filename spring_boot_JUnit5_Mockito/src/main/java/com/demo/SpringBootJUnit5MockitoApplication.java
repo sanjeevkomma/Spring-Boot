54d8e2e2b0aae33022fa761421d7151ce32272c7
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-10-16 12:37:13 +0530

    in

---
+++ spring_boot_JUnit5_Mockito/src/main/java/com/demo/SpringBootJUnit5MockitoApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJUnit5MockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJUnit5MockitoApplication.class, args);
+	}
+
+}
---
+++ spring_boot_JUnit5_Mockito/src/test/java/com/demo/SpringBootJUnit5MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJUnit5MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
