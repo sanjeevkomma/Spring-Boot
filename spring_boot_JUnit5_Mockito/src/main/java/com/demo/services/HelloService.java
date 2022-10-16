fbc38929af3d163de27c903d691e858c339771ff
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-10-16 15:32:08 +0530

    in

---
+++ spring_boot_JUnit5_Mockito/src/main/java/com/demo/services/HelloService.java
@@ -0,0 +1,7 @@
+package com.demo.services;
+
+public interface HelloService {
+
+    String get();
+
+}
---
+++ spring_boot_JUnit5_Mockito/src/main/java/com/demo/services/HelloServiceImpl.java
@@ -0,0 +1,18 @@
+package com.demo.services;
+
+import com.demo.repository.HelloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloServiceImpl implements HelloService {
+
+    @Autowired
+    HelloRepository helloRepository;
+
+    @Override
+    public String get() {
+        return helloRepository.get();
+    }
+
+}
