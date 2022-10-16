c4cc4ba61a09e61c6a1cb1d0bc43671ce50ce622
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-10-16 12:32:34 +0530

    in

--- user-dashboard-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO User VALUES (11, 10, 'Delhi', 'test@gmail.com');    
-INSERT INTO User VALUES (12, 10, 'Kanpur','test@gmail.com');    
-INSERT INTO User VALUES (13, 10, 'Lucknow', 'test@gmail.com');    
