01491226172a54adaa20f683be69edc71d4e0366
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-10-24 23:16:06 +0530

    in

--- spring_boot_MongoDB/src/test/java/com/demo/SpringBootMongoDbApplicationTests.java
+++ spring_boot_MongoDB/src/test/java/com/demo/SpringBootMongoDbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootMangoDbApplicationTests {
+class SpringBootMongoDbApplicationTests {
 
 	@Test
 	void contextLoads() {
