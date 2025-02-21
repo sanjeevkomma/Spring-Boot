40cd2b02e1a04e0049ee9c090bc57537e76718ce
sanjeev <sanjeevkomma@gmail.com>
2025-02-21 23:19:33 +0530

    in

--- spring_boot_H2_Database/src/main/java/com/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.demo.model.User;  
-
-public interface UserRepository extends CrudRepository<User, Integer>  { 
-	
-}  
