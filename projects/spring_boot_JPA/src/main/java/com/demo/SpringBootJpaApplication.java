52ecb42a95c827a2a016088d5e0f9e2f4d93e3a1
sanjeev <sanjeevkomma@gmail.com>
2025-03-01 15:53:08 +0530

    in

--- projects/spring_boot_JPA/src/main/java/com/demo/SpringBootJpaApplication.java
+++ projects/spring_boot_JPA/src/main/java/com/demo/SpringBootJpaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDataApiApplication {
+public class SpringBootJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataApiApplication.class, args);
+		SpringApplication.run(SpringBootJpaApplication.class, args);
 	}
 
 }
