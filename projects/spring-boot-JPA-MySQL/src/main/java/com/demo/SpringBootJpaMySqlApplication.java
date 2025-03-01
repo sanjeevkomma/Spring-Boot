3a8c64daeadee06b2c50c76eff257144ec7c751f
sanjeev <sanjeevkomma@gmail.com>
2025-03-01 21:53:36 +0530

    in

--- projects/spring-boot-JPA-MySQL/src/main/java/com/demo/SpringBootJpaMySqlApplication.java
+++ projects/spring-boot-JPA-MySQL/src/main/java/com/demo/SpringBootJpaMySqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJpaApplication {
+public class SpringBootJpaMySqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJpaApplication.class, args);
+		SpringApplication.run(SpringBootJpaMySqlApplication.class, args);
 	}
 
 }
