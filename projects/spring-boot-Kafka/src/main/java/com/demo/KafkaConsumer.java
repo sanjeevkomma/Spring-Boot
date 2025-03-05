29609c1602c120d46526995ef5c371c32ea09d6f
sanjeev <sanjeevkomma@gmail.com>
2025-03-06 00:09:14 +0530

    in

---
+++ projects/spring-boot-Kafka/src/main/java/com/demo/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test-topic", groupId = "my-group")
+    public void listen(ConsumerRecord<String, String> record) {
+        System.out.println("Received Message: " + record.value());
+    }
+}
---
+++ projects/spring-boot-Kafka/src/main/java/com/demo/KafkaController.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+    private final KafkaProducer kafkaProducer;
+
+    public KafkaController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        kafkaProducer.sendMessage("test-topic", message);
+        return "Message sent: " + message;
+    }
+}
+
