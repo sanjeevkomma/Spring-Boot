78c7fa7cee07356a8672376e7166d0b81703f3de
sanjeev <sanjeevkomma@gmail.com>
2025-04-06 16:23:28 +0530

    in

---
+++ projects/spring-boot-Cassandra/src/main/java/com/demo/SpringBootCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCassandraApplication.class, args);
+	}
+
+}
