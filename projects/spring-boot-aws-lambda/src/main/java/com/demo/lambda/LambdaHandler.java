68588dabc7907b02596c923208cc22e7891b00ca
sanjeev <sanjeevkomma@gmail.com>
2025-05-11 23:20:29 +0530

    in

--- projects/spring-boot-aws-lambda/src/main/java/com/demo/lambda/LambdaHandler.java
+++
@@ -1,6 +0,0 @@
-package com.demo.lambda;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class LambdaHandler extends SpringBootRequestHandler<String, String> {
-}
