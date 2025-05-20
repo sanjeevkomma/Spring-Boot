43b4a15c8f624ea75104d1bf04403fe0b02da6b1
sanjeevkomma <sanjeevkomma@gmail.com>
2025-05-20 16:53:31 +0530

    in

---
+++ projects/Microservices-DesignPatterns/spring-boot-SAGA-Choreography/src/main/java/com/demo/SpringBootSagaChoreographyApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSagaChoreographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSagaChoreographyApplication.class, args);
+	}
+
+}
