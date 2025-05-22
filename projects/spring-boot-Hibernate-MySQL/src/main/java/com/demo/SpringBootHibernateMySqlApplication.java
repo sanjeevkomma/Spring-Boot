d5ea81479bdd98d07981db533ceb96feed6d783f
sanjeevkomma <sanjeevkomma@gmail.com>
2025-05-22 14:31:49 +0530

    in

---
+++ projects/spring-boot-Hibernate-MySQL/src/main/java/com/demo/SpringBootHibernateMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateMySqlApplication.class, args);
+	}
+
+}
