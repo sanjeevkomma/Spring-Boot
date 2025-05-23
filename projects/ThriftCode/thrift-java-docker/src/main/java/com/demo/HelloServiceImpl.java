e8c50fc2f0da548ad0dd3170b465e51ddc02e6e4
sanjeevkomma <sanjeevkomma@gmail.com>
2025-05-23 17:14:55 +0530

    in

--- projects/ThriftCode/thrift-java-docker/src/main/java/com/demo/HelloServiceImpl.java
+++ projects/ThriftCode/thrift-java-docker/src/main/java/com/demo/HelloServiceImpl.java
@@ -5,6 +5,6 @@ import org.apache.thrift.TException;
 public class HelloServiceImpl implements HelloService.Iface {
     @Override
     public String sayHello(String name) throws TException {
-        return "Hello, " + name + "!";
+        return "Hello, There, " + name + "!!!!";
     }
 }
