ef4594aebc4a4921c631b6106da2e6395c780a1f
sanjeev <sanjeevkomma@gmail.com>
2025-05-25 10:40:48 +0530

    in

---
+++ projects/Database/spring-boot-JPA-Hibernate-OracleDB/src/main/java/com/demo/SpringBootJpaHibernateOracleDbApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernateOracleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernateOracleDbApplication.class, args);
+	}
+
+}
---
+++ projects/Database/spring-boot-JPA-Hibernate-PostgreSQL/src/main/java/com/demo/SpringBootJpaHibernatePostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernatePostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernatePostgreSqlApplication.class, args);
+	}
+
+}
