614c8f272eb8d1c6222ee4905c949195fda55fd4
sanjeevkomma <sanjeevkomma@gmail.com>
2025-05-26 15:32:17 +0530

    in

---
+++ projects/GCP/spring-boot-GCP-Terraform/src/main/java/com/demo/SpringBootGcpTerraformApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpTerraformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpTerraformApplication.class, args);
+	}
+
+}
