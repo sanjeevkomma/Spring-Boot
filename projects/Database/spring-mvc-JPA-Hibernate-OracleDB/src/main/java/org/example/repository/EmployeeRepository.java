a2631e34f34c5803b2238537c4ade231b3963907
sanjeevkomma <sanjeevkomma@gmail.com>
2025-05-29 20:20:57 +0530

    in

--- projects/Database/spring-mvc-JPA-Hibernate-OracleDB/src/main/java/org/example/repository/EmployeeRepository.java
+++ projects/Database/spring-mvc-JPA-Hibernate-OracleDB/src/main/java/org/example/repository/EmployeeRepository.java
@@ -3,4 +3,6 @@ package org.example.repository;
 import org.example.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
