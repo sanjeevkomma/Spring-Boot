857b9297a66ad1e94af4a0dedbbf4e57ecabe023
sanjeev <sanjeevkomma@gmail.com>
2025-05-29 22:33:13 +0530

    in

---
+++ projects/Database/spring-boot-JPA-Hibernate-DB2/src/main/java/com/demo/SpringBootJpaHibernateDb2Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernateDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernateDb2Application.class, args);
+	}
+
+}
