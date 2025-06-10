a1b42216a543fd9569a9a2b5676f7fb1f9ff90ed
sanjeev <sanjeevkomma@gmail.com>
2025-06-10 14:47:07 +0530

    in

---
+++ projects/Spring-Security/spring-boot-OAuth-TLS-HTTPS/src/main/java/com/demo/SpringBootOAuthTlsHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOAuthTlsHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOAuthTlsHttpsApplication.class, args);
+	}
+
+}
