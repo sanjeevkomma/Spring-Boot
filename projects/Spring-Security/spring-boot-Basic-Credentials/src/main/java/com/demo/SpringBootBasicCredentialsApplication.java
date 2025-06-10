8d9516d51fa22adc82b926df1b151731db2868fb
sanjeev <sanjeevkomma@gmail.com>
2025-06-10 14:40:39 +0530

    in

---
+++ projects/Spring-Security/spring-boot-Basic-Credentials/src/main/java/com/demo/SpringBootBasicCredentialsApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicCredentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicCredentialsApplication.class, args);
+	}
+
+}
---
+++ projects/Spring-Security/spring-boot-OAuth-OpenID/src/main/java/com/demo/SpringBootOAuthOpenIdApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOAuthOpenIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOAuthOpenIdApplication.class, args);
+	}
+
+}
