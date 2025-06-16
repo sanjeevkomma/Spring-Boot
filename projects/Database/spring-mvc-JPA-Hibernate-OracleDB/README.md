e5ac0b7d473149e1faf78cc02151055f1a46424d
sanjeevkomma <sanjeevkomma@gmail.com>
2025-06-16 15:32:40 +0530

    in

--- projects/Database/spring-mvc-JPA-Hibernate-OracleDB/README.md
+++
@@ -1,5 +0,0 @@
-# ToRead
-* ![image](https://github.com/user-attachments/assets/bf2712d1-f860-4a05-894f-4bf101a2642e)
-* ![image](https://github.com/user-attachments/assets/507aa000-6f37-4a05-b30a-deaf28a7b685)
-
-
--- projects/Database/spring-mvc-JPA-Hibernate-OracleDB/src/main/java/org/example/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
