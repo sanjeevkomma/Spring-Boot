9639ea184773fd08ceb984941e1d60dd0284b0b2
sanjeev <sanjeevkomma@gmail.com>
2025-08-27 22:32:16 +0530

    In

--- projects/AI/spring-boot-MCP-Shopping-Cart/src/main/java/com/demo/SpringBootMcpShoppingCartApplication.java
+++ projects/AI/spring-boot-MCP-Shopping-Cart/src/main/java/com/demo/SpringBootMcpShoppingCartApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootAiChatModelsApplication {
+public class SpringBootMcpShoppingCartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAiChatModelsApplication.class, args);
+		SpringApplication.run(SpringBootMcpShoppingCartApplication.class, args);
 	}
 
 }
