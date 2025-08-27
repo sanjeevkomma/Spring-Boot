d193991d51a47da03cc01b513a9e43c7acf7a78f
sanjeev <sanjeevkomma@gmail.com>
2025-08-27 22:24:33 +0530

    In

--- projects/AI/spring-boot-MCP/src/test/java/com/demo/SpringBootMcpApplicationTests.java
+++ projects/AI/spring-boot-MCP/src/test/java/com/demo/SpringBootMcpApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootAiApplicationTests {
+class SpringBootMcpApplicationTests {
 
 	@Test
 	void contextLoads() {
