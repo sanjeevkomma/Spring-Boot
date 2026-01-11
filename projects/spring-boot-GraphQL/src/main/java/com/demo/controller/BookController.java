663babe9987d8c00a84ce2de72ca9f286bd548d7
sanjeev <sanjeevkomma@gmail.com>
2026-01-11 22:34:04 +0530

    in

---
+++ projects/spring-boot-GraphQL/src/main/java/com/demo/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.demo.controller;
+
+import com.demo.model.Book;
+import org.springframework.graphql.data.method.annotation.*;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        books.add(new Book(1L, "Clean Code", "Robert C. Martin"));
+        books.add(new Book(2L, "Effective Java", "Joshua Bloch"));
+    }
+
+    @QueryMapping
+    public List<Book> books() {
+        return books;
+    }
+
+    @QueryMapping
+    public Book bookById(@Argument Long id) {
+        return books.stream()
+                .filter(b -> b.id().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @MutationMapping
+    public Book addBook(@Argument String title, @Argument String author) {
+        Book book = new Book((long) (books.size() + 1), title, author);
+        books.add(book);
+        return book;
+    }
+}
---
+++ projects/spring-boot-GraphQL/src/main/java/com/demo/model/Book.java
@@ -0,0 +1,3 @@
+package com.demo.model;
+
+public record Book(Long id, String title, String author) {}
