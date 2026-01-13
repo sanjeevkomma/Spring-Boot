6e321ab17ce0ad204656ec023e9bf51531c122bd
sanjeev <sanjeevkomma@gmail.com>
2026-01-13 14:59:42 +0530

    in

--- projects/spring-boot-Kotlin-POC/README.md
+++
@@ -1,2 +0,0 @@
-# REST API
-* http://localhost:8080/orders/ORD-101
--- projects/spring-boot-Kotlin-POC/src/main/kotlin/com/demo/model/Order.kt
+++
@@ -1,7 +0,0 @@
-package com.demo.model
-
-data class Order(
-    val orderId: String,
-    val productName: String,
-    val price: Double
-)
