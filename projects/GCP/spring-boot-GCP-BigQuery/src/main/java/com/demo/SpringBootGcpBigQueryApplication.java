f30a8a4e287d2e43eb464b056921303bca5176eb
sanjeev <sanjeevkomma@gmail.com>
2026-01-16 12:09:06 +0530

    in

---
+++ projects/GCP/spring-boot-GCP-BigQuery/src/main/java/com/demo/SpringBootGcpBigQueryApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpBigQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpBigQueryApplication.class, args);
+	}
+
+}
