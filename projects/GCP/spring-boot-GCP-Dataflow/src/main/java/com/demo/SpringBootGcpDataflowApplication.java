bb7939d6cfd8792607ca46b760b01476bd2a4414
sanjeev <sanjeevkomma@gmail.com>
2026-01-16 14:31:07 +0530

    in

---
+++ projects/GCP/spring-boot-GCP-Dataflow/src/main/java/com/demo/SpringBootGcpDataflowApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpDataflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpDataflowApplication.class, args);
+	}
+
+}
