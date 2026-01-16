04fc4c309cae985542339c03d5a059861260ab09
sanjeev <sanjeevkomma@gmail.com>
2026-01-16 16:21:09 +0530

    in

--- projects/GCP/spring-boot-GCP-Cloud-Spanner/src/main/java/com/demo/repository/UserRepository.java
+++ projects/GCP/spring-boot-GCP-Cloud-Spanner/src/main/java/com/demo/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.demo.model.User;
 import com.google.cloud.spring.data.spanner.repository.SpannerRepository;
 
 public interface UserRepository extends SpannerRepository<User, String> {
+
 }
