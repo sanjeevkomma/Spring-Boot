759eb486475ea1f1b6119216cd2a7a46ae6b86c1
sanjeev <sanjeevkomma@gmail.com>
2026-01-19 21:55:09 +0530

    in

---
+++ projects/spring-boot-Orchestration-POC/src/main/java/com/demo/SpringBootOrchestrationPocApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOrchestrationPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOrchestrationPocApplication.class, args);
+	}
+
+}
