ba997aa3774d21dd224a0b369f6e1e46b591712b
sanjeev <sanjeevkomma@gmail.com>
2026-01-20 17:42:01 +0530

    in

---
+++ projects/spring-boot-Elasticsearch-POC/src/main/java/com/demo/SpringBootElasticsearchPocApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearchPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearchPocApplication.class, args);
+	}
+
+}
