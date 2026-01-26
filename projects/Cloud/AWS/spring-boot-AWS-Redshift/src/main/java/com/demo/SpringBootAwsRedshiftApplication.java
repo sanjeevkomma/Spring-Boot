d72902be0d8ca74c5198ba112cd020498f223ae4
sanjeev <sanjeevkomma@gmail.com>
2026-01-26 12:29:48 +0530

    in

---
+++ projects/Cloud/AWS/spring-boot-AWS-Redshift/src/main/java/com/demo/SpringBootAwsRedshiftApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsRedshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsRedshiftApplication.class, args);
+	}
+
+}
